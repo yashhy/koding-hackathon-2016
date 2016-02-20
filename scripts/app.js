7c95c982bd6e2e763e2acd90dc7228400d0ecdac
Hentry Martin <phentrymartin@gmail.com>
2016-02-20 15:19:41 +0530

    Base router

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,13 @@
-var app = angular.module('invoiceManager', []);
+(function () {
+    angular.module('invoiceManager', ['ui.router'])
+    .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+
+        $stateProvider.state('login', {
+            url : '/login',
+            templateUrl : 'views/login.html'
+        });
+        $urlRouterProvider.otherwise('/login');
+    }]);
+})();
+
 
